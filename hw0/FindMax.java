d73e8585b4a5f39f64a051daf22da25a56cda80b
rizky <rizky.jayusman96@gmail.com>
2021-10-31 20:35:21 +0700

    exercise: find max with for loop

--- hw0/FindMax.java
+++ hw0/FindMax.java
@@ -14,8 +14,20 @@ public class FindMax {
         return max;
     }
 
+    public static int maxWithFor(int [] m) {
+        int max = 0;
+        for(int x = 0; x < m.length; x++) {
+            if(max < m[x]) {
+                max = m[x];
+            }
+        }
+
+        return max;
+    }
+
     public static void main(String [] args) {
         int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
-        System.out.println(max(numbers));
+        // System.out.println(max(numbers));
+        System.out.println(maxWithFor(numbers));
     }
 }
