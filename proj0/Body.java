1a6bada3ee2a6699de4dbfefdc4fa6fab4205d68
rizky <rizky.jayusman96@gmail.com>
2021-11-01 12:53:28 +0700

    proj 0: add body class and constructor

---
+++ proj0/Body.java
@@ -0,0 +1,27 @@
+public class Body {
+
+    public double xxPos;
+
+    public double yyPos;
+
+    public double xxVel;
+
+    public double yyVel;
+
+    public double mass;
+
+    public String imgFileName;
+
+    public Body(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Body(Body b) {
+        this(b.xxPos, b.yyPos, b.xxVel, b.yyVel, b.mass, b.imgFileName);
+    }
+}
