62632fef3669c12f532dccda00b3a5f727321f1a
rizky <rizky.jayusman96@gmail.com>
2021-11-01 17:00:26 +0700

    proj 0 : read body

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -5,4 +5,17 @@ public class NBody {
         double radius = in.readDouble();
         return radius;
     }
+
+    
+    public static Body[] readBodies(String filename) {
+        In in = new In(filename);
+        int arrLength = in.readInt();
+        double radius = in.readDouble();
+        Body[] arr = new Body[arrLength];
+        for(int i = 0; i < arrLength; i++) {
+            arr[i] = new Body(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readString());
+        }
+
+        return arr;
+    }
 }
