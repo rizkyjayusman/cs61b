362a1350ba6ee80c08e857c9b1aee063e8aa1bb4
rizky <rizky.jayusman96@gmail.com>
2021-11-02 15:46:23 +0700

    w2.1 : challenge IntList add dincrList

--- w2.1/ExtraIntListPractice.java
+++ w2.1/ExtraIntListPractice.java
@@ -15,8 +15,12 @@ public class ExtraIntListPractice {
       * each element incremented by x. Not allowed to use
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
-        /* Your code here. */
-        return L;
+        IntList ref = null;
+        if(L.rest != null) {
+            ref = dincrList(L.rest, x);
+        }
+
+        return new IntList(L.first - x, ref);
     }
 
     public static void main(String[] args) {
@@ -24,17 +28,36 @@ public class ExtraIntListPractice {
         L.rest = new IntList(7, null);
         L.rest.rest = new IntList(9, null);
 
-        System.out.println(L.size());
-        System.out.println(L.iterativeSize());
+        System.out.println("L size before copy to N => " + L.size());
+        System.out.println("L size before copy to N using iterative => " + L.iterativeSize());
+        System.out.println("L before copy to N");
+        L.print();
 
         // Test your answers by uncommenting. Or copy and paste the
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
         // System.out.println(incrList(L, 3));
         IntList N = incrList(L, 3);
-        System.out.println(N.size());
-        System.out.println(N.iterativeSize());
+        System.out.println("L size after copy to N => " + L.size());
+        System.out.println("L size after copy copy to N using iterative => " + L.iterativeSize());
+        System.out.println("L does not changed");
+        L.print();
+
+        System.out.println("N size => " + N.size());
+        System.out.println("N size using iterative => " + N.iterativeSize());
+        System.out.println("N identical to L but L does not changed");
         N.print();
-        System.out.println(dincrList(L, 3));        
+
+        IntList M = dincrList(L, 3);
+        System.out.println("L size after copy to M => " + L.size());
+        System.out.println("L size after copy to M using iterative => " + L.iterativeSize());
+        System.out.println("L does not changed");
+        L.print();
+
+        System.out.println("M size => " + M.size());
+        System.out.println("M size using iterative => " + M.iterativeSize());
+        System.out.println("M identical to L but L does not changed");
+        M.print();
+
     }
 }
