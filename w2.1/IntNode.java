26fab1103bd1fbe7754201e740e201d02bc88957
rizky <rizky.jayusman96@gmail.com>
2021-11-03 11:56:40 +0700

    w2.2 : add node for single link list

---
+++ w2.1/IntNode.java
@@ -0,0 +1,9 @@
+public class IntNode {
+    public int item;
+    public IntNode next;
+
+    public IntNode(int item, IntNode next) {
+        this.item = item;
+        this.next = next;
+    }
+}
